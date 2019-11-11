0fd4c34a727cdba3ed23981d4a9eaefc15e3438f
Ajayi Adeyemi <aajayiadeyemi@gmail.com>
2019-11-11 20:40:38 +0100

    snytaxbug fixes

--- restapi/main.go
+++ restapi/main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
-	// "encoding/json"
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"html/template"
 	"net/http"
 )
- 
+
+// Article struct
 type Article struct{
-	Title string `json"title"`
-	Desc string `json"desc"`
-	Content string `json"content"`
+	Title string `json:"title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
 }
 
+// Articles array
+type Articles []Article
+
+func allArticle(w http.ResponseWriter, r *http.Request)  {
+	articles := Articles{
+		Article{Title: "first title", Desc: "article description", Content: "lorem ipsum dolar"},
+	}
+
+	json.NewEncoder(w).Encode(articles)
+
+}
 
 func homePage(w http.ResponseWriter, r *http.Request)  {
 	tmplt := template.Must(template.ParseFiles("home.html"))
@@ -30,6 +42,7 @@ func handleRequest()  {
 	r := mux.NewRouter()
 	r.HandleFunc("/home", homePage)
 	r.HandleFunc("/about", aboutPage)
+	r.HandleFunc("/articles", allArticle)
 
 	http.ListenAndServe(":8080", r)
 }
