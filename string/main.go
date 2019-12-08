a6a812c9824f036adda648f92cc3b6207ee93656
Ajayi Adeyemi <aajayiadeyemi@gmail.com>
2019-12-08 16:44:17 +0100

    name to upper case

--- string/main.go
+++ string/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"strings"
+	"os"
 	"unicode/utf8"
 	"fmt"
 )
 
 func main() {
-	name := "ajayi adeyemi"
-	fmt.Println(name)
-	fmt.Println(len(name))
-	// best way to determin the lenght of a string
-	fmt.Println(utf8.RuneCountInString(name))
+	// name := "ajayi adeyemi"
+	// fmt.Println(name)
+	// fmt.Println(len(name))
+	// // best way to determin the lenght of a string
+	// fmt.Println(utf8.RuneCountInString(name))
+
+	word := os.Args[1]
+	l := utf8.RuneCountInString(word)
+	output := word + strings.Repeat("!", l )
+	output = strings.ToUpper(output)
+	fmt.Println(output)
 }
